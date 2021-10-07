ed691fc5fec3e8d2f7d0cb6414cd094fb896ad41
jocon21 <92104564+jocon21@users.noreply.github.com>
2021-10-07 09:23:24 -0600

    Add files via upload

---
+++ Examen/js/verificar.js
@@ -0,0 +1,11 @@
+function go(){
+    if (document.formingreso.usuario.value=='maria' && document.formingreso.contraseña.value=='examen');
+    {
+        document.formingreso.subwit ();
+    }
+    else
+    {
+        alert("Ingrese su Usuario y Contraseña correctamente");
+    }
+}
+
